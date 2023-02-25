a033f1ef88c8edc14340f885ad972f5acd286b3a
VictorGNetto <victornetto@discente.ufg.br>
2023-02-25 15:27:23 -0300

    Correção no número de nós

--- constMP.py
+++ constMP.py
@@ -1,7 +1,7 @@
 #To Do: Replace all this with a naming service
 PEERS = ['10.162.0.2', '10.158.0.2', '10.138.0.2', '10.166.0.2', '10.200.0.2', '10.208.0.2', '10.190.0.2', '10.140.0.2']
 PORT = 4567
-N = 6
+N = 8
 N_MSGS = 10
 SERVER_ADDR = '10.128.0.4'
 SERVER_PORT = 4568
