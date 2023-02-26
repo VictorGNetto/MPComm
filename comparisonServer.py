bd60725b0057c9acd01bfe22066589d70c51a949
VictorGNetto <victornetto@discente.ufg.br>
2023-02-26 11:36:13 -0300

    Ajsute no script de comparação para fazer comparações sucessivas

--- comparisonServer.py
+++ comparisonServer.py
@@ -6,26 +6,34 @@ serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind((SERVER_ADDR, SERVER_PORT))
 serverSock.listen(5)
 
-numMsgs = 0
-msgs = [] # each msg is a list of tuples (with the original messages received by the peer processes)
+def compare():
+	numMsgs = 0
+	msgs = [] # each msg is a list of tuples (with the original messages received by the peer processes)
 
-# Receive the lists of messages from the peer processes
-while numMsgs < N:
-	(conn, addr) = serverSock.accept()
-	msgPack = conn.recv(2048)
-	conn.close()
-	msgs.append(pickle.loads(msgPack))
-	numMsgs = numMsgs + 1
+	# Receive the lists of messages from the peer processes
+	while numMsgs < N:
+		(conn, addr) = serverSock.accept()
+		msgPack = conn.recv(2048)
+		conn.close()
+		msgs.append(pickle.loads(msgPack))
+		numMsgs = numMsgs + 1
 
-unordered = 0
+	unordered = 0
 
-# Compare the lists of messages
-for j in range(0,N_MSGS-1):
-	firstMsg = msgs[0][j]
-	for i in range(1,N-1):
-		if firstMsg != msgs[i][j]:
-			unordered = unordered + 1
-			break
-	
-print ('Found ' + str(unordered) + ' unordered message rounds')
+	# Compare the lists of messages
+	for j in range(0,N_MSGS-1):
+		firstMsg = msgs[0][j]
+		for i in range(1,N-1):
+			if firstMsg != msgs[i][j]:
+				unordered = unordered + 1
+				break
+		
+	print ('Found ' + str(unordered) + ' unordered message rounds')
+
+# this code must be ended with a Crtl + C
+while True:
+	compare()
+
+# TODO: The code execution never reaches the line bellow.. find a better way
+# to close the socket
 serverSock.close()
